a97513be073e012b63d2e919aa75d0de4022efde
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-06-29 11:38:42 -0400

    calling the html id-s to script js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// DOM elements
+let questionsEl = document.querySelector("#questions");
+let timerEl = document.querySelector("#time");
+let choicesEl = document.querySelector("#choices");
+let submitBtn = document.querySelector("#submit");
+let startBtn = document.querySelector("#start");
+let initialsEl = document.querySelector("#initials");
+let feedbackEl = document.querySelector("#feedback");
