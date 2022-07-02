88094e5e902d6126f8ed194e9bcadd8c439945f9
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-07-01 21:08:37 -0400

    script update

---
+++ js/script.js
@@ -0,0 +1,139 @@
+// DOM elements
+let questionsEl = document.querySelector("#questions");
+let timerEl = document.querySelector("#time");
+let choicesEl = document.querySelector("#choices");
+let submitBtn = document.querySelector("#submit");
+let startBtn = document.querySelector("#start");
+let initialsEl = document.querySelector("#initials");
+let feedbackEl = document.querySelector("#feedback");
+
+// states of quistions' var.
+// hide start screen and un hide quistion
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+ 
+  questionsEl.removeAttribute("class");
+
+  // timer display starting time 
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+// get current object from array, load next quistion, clear  and choices
+function getQuestion() {
+  
+  var currentQuestion = questions[currentQuestionIndex];
+
+  
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+// clear out any old question choices and loop
+// create new button for each choice
+  choicesEl.innerHTML = "";
+
+  currentQuestion.choices.forEach(function(choice, i) {
+
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // attach event listeners and appendChild
+    choiceNode.onclick = questionClick;
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+// is it the wrong choice? and penalty of time and feedback
+function questionClick() {
+  
+    if (this.value !== questions[currentQuestionIndex].answer) {
+  
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+      timerEl.textContent = time;
+      feedbackEl.textContent = "Wrong!";
+      feedbackEl.style.color = "red";
+      feedbackEl.style.fontSize = "400%";
+    } else {
+      feedbackEl.textContent = "Correct!";
+      feedbackEl.style.color = "green";
+      feedbackEl.style.fontSize = "400%";
+    }
+  
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // next question and time check
+    currentQuestionIndex++;
+  
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+  //endquiz and stop timmer
+  function quizEnd() {
+   
+    clearInterval(timerId);
+  
+    // end screen/final score 
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+  
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+  
+    //set attribute name: hide
+    questionsEl.setAttribute("class", "hide");
+  }
+  //check time statues
+  function clockTick() {
+  
+    time--;
+    timerEl.textContent = time;
+  
+  
+    if (time <= 0) {
+      quizEnd();
+    }
+  }
+  
+  function saveHighscore() {
+    // get value, get saved score from storage, rewrite score and save to localstorage.
+    var initials = initialsEl.value.trim();
+    //if not , set to empty array
+    if (initials !== "") {
+
+        var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      var newScore = {
+        score: time,
+        initials: initials
+      };
+  
+      // save to localstorage
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+      // point to score.html
+      window.location.href = "score.html";
+    }
+  }
+
